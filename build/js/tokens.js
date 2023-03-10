7ae2c875c1a5088daec8819d50c742d1fb55c4c2
eduardo1199 <eduardo1199@users.noreply.github.com>
2023-03-10 13:37:10 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,23 +1,23 @@
 /**
  * Do not edit directly
- * Generated on Fri, 10 Mar 2023 12:38:31 GMT
+ * Generated on Fri, 10 Mar 2023 13:37:09 GMT
  */
 
 module.exports = {
-  ColorBaseWhite: '#ffffff',
-  ColorBaseBackground: '#fafafa',
-  ColorBaseCard: '#f3f2f2',
-  ColorBaseInput: '#ededed',
-  ColorBaseButton: '#e6e5e5',
-  ColorBaseHover: '#d7d5d5',
-  ColorBaseLabel: '#8d8686',
-  ColorBaseText: '#574f4d',
-  ColorBaseSubtitle: '#403937',
-  ColorBaseTitle: '#272221',
-  ColorBrandPurpleDark: '#4b2995',
-  ColorBrandPurple: '#8047f8',
-  ColorBrandPurpleLight: '#ebe5f9',
-  ColorBrandYellowDark: '#c47f17',
-  ColorBrandYellow: '#dbac2c',
-  ColorBrandYellowLight: '#f1e9c9',
-}
+  "ColorBaseWhite": "#ffffff",
+  "ColorBaseBackground": "#fafafa",
+  "ColorBaseCard": "#f3f2f2",
+  "ColorBaseInput": "#ededed",
+  "ColorBaseButton": "#e6e5e5",
+  "ColorBaseHover": "#d7d5d5",
+  "ColorBaseLabel": "#8d8686",
+  "ColorBaseText": "#574f4d",
+  "ColorBaseSubtitle": "#403937",
+  "ColorBaseTitle": "#272221",
+  "ColorBrandPurpleDark": "#4b2995",
+  "ColorBrandPurple": "#8047f8",
+  "ColorBrandPurpleLight": "#ebe5f9",
+  "ColorBrandYellowDark": "#c47f17",
+  "ColorBrandYellow": "#dbac2c",
+  "ColorBrandYellowLight": "#f1e9c9"
+};
