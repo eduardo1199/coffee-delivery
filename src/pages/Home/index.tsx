918373a4e4a3827f4addc39b8c55e30ca9695536
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-14 09:32:32 -0300

    created section main about of coffee delivery

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,65 @@
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import {
+  Container,
+  Content,
+  GridService,
+  SectionAbout,
+  ServiceContainer,
+  ProductsContainer,
+  CoffeeContainer,
+} from './styles'
+
+import { ShoppingCart, Timer, Package, Coffee } from 'phosphor-react'
+
+import CoffeeImg from '../../assets/coffee.svg'
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <div>Content</div>
+
+      <Content>
+        <SectionAbout>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+          <GridService>
+            <ServiceContainer variant="ColorBrandYellowDark">
+              <div>
+                <ShoppingCart />
+              </div>
+              <span>Compra simples e segura</span>
+            </ServiceContainer>
+            <ServiceContainer variant="ColorBaseText">
+              <div>
+                <Package />
+              </div>
+              <span>Embalagem mantém o café intacto</span>
+            </ServiceContainer>
+            <ServiceContainer variant="ColorBrandYellow">
+              <div>
+                <Timer />
+              </div>
+              <span>Entrega rápida e rastreada</span>
+            </ServiceContainer>
+            <ServiceContainer variant="ColorBrandPurple">
+              <div>
+                <Coffee />
+              </div>
+              <span>O café chega fresquinho até você</span>
+            </ServiceContainer>
+          </GridService>
+        </SectionAbout>
+        <img src={CoffeeImg} alt="" />
+      </Content>
+
+      <ProductsContainer>
+        <h1>Nossos cafés</h1>
+
+        <CoffeeContainer></CoffeeContainer>
+      </ProductsContainer>
     </Container>
   )
 }
